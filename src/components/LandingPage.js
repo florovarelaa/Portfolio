596fc7742f0848086c7580fd99bf44248eee730e
florovarelaa <florovarelaa@gmail.com>
2019-02-20 00:45:53 -0300

    Creating Landing Page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render () {
         return(
-            <div>
-                <h1>Landing Page</h1>
+            <div sytle={{width: '100%', margin:'auto'}}>
+                <Grid className='landing-grid'>
+                    <Cell col={12}>
+                        <img 
+                            src='https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png'
+                            alt='avatar'
+                            className='avatar-img'
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+                            <hr />
+                            <p>HTML/CSS | Javascript | Bootstrap | React </p>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
