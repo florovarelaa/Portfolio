bcd1ef598cc86b0bd443226b92c59ec14e3d7fa4
florovarelaa <florovarelaa@gmail.com>
2019-02-19 23:46:30 -0300

    Initial Layout - added react-mdl and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Main from './components/Main'
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello world</h1>
+          <div className="demo-big-content">
+        <Layout>
+            <Header title="Title" scroll>
+                <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main />
+            </Content>
+        </Layout>
       </div>
     );
   }
