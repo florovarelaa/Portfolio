56b32230b0f4efdc35bdb478010108d465a3db76
florovarelaa <florovarelaa@gmail.com>
2019-02-27 20:16:19 -0300

    Contacts Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 import './Contact.css';
 
 class Contact extends Component {
@@ -7,7 +7,8 @@ class Contact extends Component {
         return(
             <div className="contact-body">
                 <Grid className="contact-grid">
-                    <Cell col={6}>
+                    {/* Left Side */}
+                    <Cell className="contact-left" col={6}>
                         <h2>Florencio Varela</h2>
                         <img className="contact-img"
                             src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
@@ -18,9 +19,22 @@ class Contact extends Component {
                         Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
                         </p>
                     </Cell>
+                    {/* Right Side */}
                     <Cell col={6}>
-
-                    </Cell>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <List>
+                            <ListItem>
+                                <ListItemContent icon="person">Bryan Cranston</ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                            </ListItem>
+                        </List>
+                    </Cell> 
                 </Grid>
             </div>
         )
