780c20149d473b3df70a2b2ab572bb9852b6ac1f
Florencio <florovarelaa@gmail.com>
2019-03-29 15:51:13 -0300

    Adding classes to navBar

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { Link } from 'react-router-dom';
 import Blur from 'react-css-blur'
 
 class App extends Component {
-
-  state = {
-    blurOn: false,
-    blurTime: 500,
-    addClass: false,
-  };
-
+  
+  constructor(props) {
+    super(props);
+    this.state = {addClass: false,
+      blurOn: false,
+      blurTime: 500,
+    }
+  }
+  
   blurOn = () => {
     this.setState({ blurOn: true });
     setTimeout(() => {
@@ -25,11 +27,11 @@ class App extends Component {
   toggleClass() {
     this.setState({addClass: !this.state.addClass});
   }
-
+  
   render() {
     let classToAdd = ["navigation-link"];
     if (this.state.addClass) {
-      classToAdd.push("selected")
+      classToAdd.push("selected");
     }
     return (
       <div className="demo-big-content">
@@ -38,16 +40,21 @@ class App extends Component {
         <Blur radius={ this.state.blurOn ? '5px' : '0' } transition={`${this.state.blurTime}ms`}>
         <Layout>
             <Header className="header-color" title=" " scroll>
-                <Navigation className={`header-navigation ${classToAdd.join('-')}`} onClick={ () => {
-                                                                    this.blurOn();
-                                                                    this.toggleClass.bind(this);
-                                                                    }
-                                                                  }
-                >
-                        <Link className='navigation-link' to="/home" >Home</Link>
-                        <Link className='navigation-link' to="/about">About</Link>
-                        <Link className='navigation-link' to="/projects">Projects</Link>
-                        <Link className='navigation-link' to="/contact">Contact</Link>
+                <Navigation className='header-navigation' 
+                    onClick={ (event) => {
+                      // this.toggleClass();
+                      this.blurOn();
+                      // classToAdd.join('-');
+                      }
+                    }
+                    >
+                        <Link className={`navigation-link ${classToAdd.join('-')}`} to="/home" onClick= {() => {
+                          this.toggleClass();
+                          classToAdd.join('-');
+                        }}>Home</Link>
+                        <Link className={`navigation-link ${classToAdd.join('-')}`} to="/home" to="/about">About</Link>
+                        <Link className={`navigation-link ${classToAdd.join('-')}`} to="/home" to="/projects">Projects</Link>
+                        <Link className={`navigation-link ${classToAdd.join('-')}`} to="/home" to="/contact">Contact</Link>
                 </Navigation>
             </Header>
             <Content>
