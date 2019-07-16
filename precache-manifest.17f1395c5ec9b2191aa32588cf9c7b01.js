d947cfbb2c6c3dc19a14db4c0a0848b9791f4997
florovarelaa <florovarelaa@gmail.com>
2019-07-16 20:18:46 -0300

    Updates

---
+++ precache-manifest.17f1395c5ec9b2191aa32588cf9c7b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ac8d70261a126085336",
+    "url": "/React-portfolio/static/js/runtime~main.0ac8d702.js"
+  },
+  {
+    "revision": "8afb3795348de587f455",
+    "url": "/React-portfolio/static/js/main.8afb3795.chunk.js"
+  },
+  {
+    "revision": "871f4386c2ce6393bb0f",
+    "url": "/React-portfolio/static/js/2.871f4386.chunk.js"
+  },
+  {
+    "revision": "8afb3795348de587f455",
+    "url": "/React-portfolio/static/css/main.2b7e968d.chunk.css"
+  },
+  {
+    "revision": "871f4386c2ce6393bb0f",
+    "url": "/React-portfolio/static/css/2.e349e694.chunk.css"
+  },
+  {
+    "revision": "5fc0eb01eaef97f41da90941b13fc195",
+    "url": "/React-portfolio/index.html"
+  }
+];
