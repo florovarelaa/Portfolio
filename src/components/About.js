3c8e21b4a75aee327a8aa4aed4931c159a02add8
florovarelaa <florovarelaa@gmail.com>
2019-09-05 02:24:21 -0300

    Updated About Section and personal description

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,11 @@ class About extends Component {
             // <div className="about-banner-text">
             <Banner>
                     <p className="about-banner-text">Hello, my name is <span className="about-name">Florencio Varela</span>.</p>
-                    <p className="about-banner-text">I'm an <span>Informatic Engineering</span> student and <span> Developer</span> from Argentina.</p>
+                    <p className="about-banner-text">I'm an <span>Informatic Engineering</span> and <span> Developer</span> from Argentina.</p>
                     <p className="about-banner-text">I'm quietly confident, naturally curious, and perpetually improving my skills. I design and code beautiful simple things, and I <span>love what I do</span>.</p>
                     <p className="about-banner-text">I have managed different projects. This role allowed me to develop <span>Management Knowledge</span>, experience and skills to bring projects to successful completion. It also encouraged me to prepare the <span>PMP Exam</span>.</p>
                     <p className="about-banner-text">As a developer I enjoy building <span>Fast & Responsive</span> applications.</p>
-                    <p className="about-banner-text">I started my career as a QA tester.
+                    <p className="about-banner-text">I started my career as a QA tester. 
                     This brought me the possibility to develop different <span>skills</span> such as:</p>
                     <ul className="about-banner-text">
                         <li>Problem-Solving Skills</li>
@@ -21,6 +21,7 @@ class About extends Component {
                         <li>Experience with Agile Development Process</li>
                     </ul>
                     <p className="about-banner-text">After obtaining <span>Knowledge & Experience</span> as a member of the QA team I migrated to building things!</p>
+                    <p className="about-banner-text">Later on, I began to <span>Manage</span> different <span>Team</span> which included <span>Members</span> with different skills.</p>
                     <p className="about-banner-text">If I had to describe myself with one sentence it would be:</p>
                     <p className="about-banner-text"><span>Learn to Love to Learn</span></p>
                     <p className="about-banner-text">My <span>Interests</span> are: Javascript, VR/AR, Machine Learning, IA</p>
